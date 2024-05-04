ccd64a2b9879734c47e12f82f9a4cfae4c9ec9c4
MatsVingerhoets <mats.vingerhoets5@gmail.com>
2024-05-05 01:43:33 +0200

    added profile image

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ import { Props } from "./types"
 const Button = ({ children, onClick }: PropsWithChildren<Props>) => {
   return (
     <button
-      className="bg-accent-default text-background-primary font-bold py-2 px-4 rounded shadow-3xl"
+      className="bg-accent-default text-background-primary font-bold py-2 px-4 rounded shadow-3xl w-fit"
       onClick={onClick}
     >
       {children}
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,5 +1,6 @@
 import { createLazyFileRoute } from "@tanstack/react-router"
 import Button from "src/components/Button"
+import Image from "src/assets/charizard.png"
 
 export const Route = createLazyFileRoute("/")({
   component: Index
@@ -8,15 +9,20 @@ export const Route = createLazyFileRoute("/")({
 function Index() {
   return (
     <div className="p-2">
-      <h1 className="text-foreground-primary mb-9">
-        Hello, I am Mats Vingerhoets!
-      </h1>
-      <p className="text-foreground-primary mb-7">
-        I’m a full stack developer with over 5 years of experience in the field.
-        My passion for coding grows everyday and I’m always on the look for new
-        challenges!
-      </p>
-      <Button>Let's talk!</Button>
+      <div className='flex align-middle'>
+        <div className="flex flex-col justify-between min-h-full">
+          <h1 className="text-foreground-primary">
+            Hello, I am Mats Vingerhoets!
+          </h1>
+          <p className="text-foreground-primary">
+            I’m a full stack developer with over 5 years of experience in the field.
+            My passion for coding grows everyday and I’m always on the look for new
+            challenges!
+          </p>
+          <Button>Let's talk!</Button>
+        </div>
+        <img className="w-52 rounded-full shadow-3xl" src={Image}></img>
+      </div>
     </div>
   )
 }
